143eb536a4397013a22e94baef8c3e733dd37375
webklex <info@webklex.com>
2022-09-10 20:39:29 +0200

    Log level support added to suppress noise

--- log/log.go
+++ log/log.go
@@ -5,13 +5,25 @@ import (
 	"github.com/fatih/color"
 )
 
+const (
+	LogAll       = 0
+	LogSuccess   = 1
+	LogWarning   = 2
+	LogStatistic = 3
+	LogError     = 4
+)
+
+var Mode = LogAll
+
 //
 // Info
 // @Description: Print an info line
 // @param format string
 // @param args ...interface{}
 func Info(format string, args ...interface{}) {
-	Log(color.FgCyan, "info", format, args...)
+	if Mode <= LogAll {
+		Log(color.FgCyan, "info", format, args...)
+	}
 }
 
 //
@@ -20,13 +32,15 @@ func Info(format string, args ...interface{}) {
 // @param format interface{}
 // @param args ...interface{}
 func Error(format interface{}, args ...interface{}) {
-	switch format.(type) {
-	case error:
-		Log(color.FgRed, "error", format.(error).Error(), args...)
-	case string:
-		Log(color.FgRed, "error", format.(string), args...)
-	default:
-		Log(color.FgRed, "error", fmt.Sprintf("%v", format), args...)
+	if Mode <= LogAll || Mode == LogError {
+		switch format.(type) {
+		case error:
+			Log(color.FgRed, "error", format.(error).Error(), args...)
+		case string:
+			Log(color.FgRed, "error", format.(string), args...)
+		default:
+			Log(color.FgRed, "error", fmt.Sprintf("%v", format), args...)
+		}
 	}
 }
 
@@ -36,7 +50,9 @@ func Error(format interface{}, args ...interface{}) {
 // @param format string
 // @param args ...interface{}
 func Warning(format string, args ...interface{}) {
-	Log(color.FgYellow, "warning", format, args...)
+	if Mode <= LogAll || Mode == LogWarning {
+		Log(color.FgYellow, "warning", format, args...)
+	}
 }
 
 //
@@ -45,7 +61,15 @@ func Warning(format string, args ...interface{}) {
 // @param format string
 // @param args ...interface{}
 func Success(format string, args ...interface{}) {
-	Log(color.FgGreen, "success", format, args...)
+	if Mode <= LogAll || Mode == LogSuccess {
+		Log(color.FgGreen, "success", format, args...)
+	}
+}
+
+func Statistic(format string, args ...interface{}) {
+	if Mode <= LogAll || Mode == LogStatistic {
+		Log(color.FgMagenta, "statistic", format, args...)
+	}
 }
 
 //
--- main.go
+++ main.go
@@ -25,6 +25,7 @@ func main() {
 	flag.CommandLine.BoolVar(&a.LocalOnly, "local", a.LocalOnly, "Only use local files. Don't perform any requests")
 	flag.CommandLine.BoolVar(&a.Combined, "combined", a.Combined, "Combine all source files into one")
 	flag.CommandLine.BoolVar(&a.DisableSSL, "disable-ssl", a.DisableSSL, "Don't verify the site's SSL certificate")
+	flag.CommandLine.IntVar(&log.Mode, "log", log.Mode, "Set the log mode (0 = all, 1 = success, 2 = warning, 3 = statistic, 4 = error)")
 	flag.CommandLine.BoolVar(&a.DangerouslyWritePaths, "dangerously-write-paths", a.DangerouslyWritePaths, "Write full paths. WARNING: Be careful here, you are pulling directories from an untrusted source")
 
 	sv := flag.Bool("version", false, "Show version and exit")
